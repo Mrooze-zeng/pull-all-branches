e1cfb0b7f6dcee8c9c1134bb22aa1c89be67578a
John <mrooze@v-hiker.cn>
2021-11-12 17:18:11 +0800

    add statusbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const { commands, window, workspace } = require("vscode");
+const { commands, window, workspace, StatusBarAlignment } = require("vscode");
 
 const Git = require("./git");
+const commadId = "pull-all.pull";
 
 function activate(context) {
   const currentFolder = workspace.workspaceFolders[0];
   const isGitExist = Git.checkGit();
+
   let message = "There is no git here!";
   let commad = function () {
     window.showInformationMessage(message);
@@ -13,14 +15,14 @@ function activate(context) {
     try {
       const projectRoot = Git.getProjectRoot(currentFolder.uri.path);
       const git = new Git({ cwd: projectRoot });
+      initStatusBar(context);
       commad = function () {
         const currentBranch = git.getCurrentBranch();
         const updatedBranch = [];
         git.fetch();
         git.getBranches().forEach((branch) => {
-          const [local] = branch;
-          const count = git.getRemoteCommitCount(...branch);
-          if (count) {
+          const [local, remote] = branch;
+          if (remote && git.getRemoteCommitCount(...branch)) {
             updatedBranch.push(local);
             git.checkout(local);
             git.pull();
@@ -38,9 +40,18 @@ function activate(context) {
       };
     } catch (error) {}
   }
-  let disposable = commands.registerCommand("pull-all.pull", commad);
+  let disposable = commands.registerCommand(commadId, commad);
 
   context.subscriptions.push(disposable);
+}
+
+function initStatusBar(context) {
+  const statusBar = window.createStatusBarItem(StatusBarAlignment.Left, 100);
+  statusBar.command = commadId;
+  context.subscriptions.push(statusBar);
+  statusBar.text = `$(arrow-down) PULL ALL`;
+  statusBar.tooltip = "Pull all remote update!";
+  statusBar.show();
 }
 
 function deactivate() {}
