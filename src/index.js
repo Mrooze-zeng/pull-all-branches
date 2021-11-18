d2645341bae8bf7a234e78f8c555b77d2c49ec8c
John <mrooze@v-hiker.cn>
2021-11-18 12:33:19 +0800

    init extension

--- src/index.js
+++ src/index.js
@@ -11,69 +11,74 @@ const commadId = "pull-all.pull";
 const tooltip = "Pull all remote update!";
 
 function activate(context) {
-  const currentFolder = workspace.workspaceFolders[0];
-  const isGitExist = Git.checkGit();
+  if (workspace.workspaceFolders) {
+    const currentFolder = workspace.workspaceFolders[0];
+    const isGitExist = Git.checkGit();
 
-  let message = "There is no git here!";
-  let commad = function () {
-    window.showInformationMessage(message);
-  };
-  if (isGitExist) {
-    try {
-      const projectRoot = Git.getProjectRoot(currentFolder.uri.path);
-      const git = new Git({ cwd: projectRoot });
-      initStatusBar(context);
-      commad = function () {
-        window.withProgress(
-          {
-            location: ProgressLocation.Notification,
-            title: tooltip,
-            cancellable: false,
-          },
-          async function (progress) {
-            progress.report({ increment: 5 });
-            git.logger(
-              `============run on ${new Date().toString()}==============`,
-            );
-            const currentBranch = git.getCurrentBranch();
-            const updatedBranch = [];
-            git.fetch();
-            progress.report({ increment: 10, message: "Start pulling..." });
-            const branches = git.getBranches();
-            let step = Math.floor(80 / branches.length);
-            branches.forEach((branch) => {
-              const [local, remote] = branch;
-              if (remote && git.getRemoteCommitCount(...branch)) {
-                updatedBranch.push(local);
-                git.checkout(local);
-                git.pull();
+    let message = "There is no git here!";
+    let commad = function () {
+      window.showInformationMessage(message);
+    };
+    if (isGitExist) {
+      try {
+        const projectRoot = Git.getProjectRoot(currentFolder.uri._fsPath);
+        const git = new Git({ cwd: projectRoot });
+        initStatusBar(context);
+        commad = function () {
+          window.withProgress(
+            {
+              location: ProgressLocation.Notification,
+              title: tooltip,
+              cancellable: false,
+            },
+            async function (progress) {
+              progress.report({ increment: 5 });
+              git.logger(
+                `============run on ${new Date().toString()}==============`,
+              );
+              const currentBranch = git.getCurrentBranch();
+              const updatedBranch = [];
+              git.fetch();
+              progress.report({ increment: 10, message: "Start pulling..." });
+              const branches = git.getBranches();
+              let step = Math.floor(80 / branches.length);
+              branches.forEach((branch) => {
+                const [local, remote] = branch;
+                if (remote && git.getRemoteCommitCount(...branch)) {
+                  updatedBranch.push(local);
+                  git.checkout(local);
+                  git.pull();
+                }
+                progress.report({
+                  increment: step,
+                  message: `Pulling ${local}`,
+                });
+              });
+              git.checkout(currentBranch);
+              message = `There is no branch updated!`;
+              if (updatedBranch.length) {
+                message = `${
+                  updatedBranch.length
+                } branch(es) updated:\n${updatedBranch.join("\n")}`;
               }
-              progress.report({ increment: step, message: `Pulling ${local}` });
-            });
-            git.checkout(currentBranch);
-            message = `There is no branch updated!`;
-            if (updatedBranch.length) {
-              message = `${
-                updatedBranch.length
-              } branch(es) updated:\n${updatedBranch.join("\n")}`;
-            }
 
-            progress.report({
-              increment: branches.length ? 10 : 90,
-              message: message,
-            });
-            window.showInformationMessage(message);
-            return;
-          },
-        );
-      };
-    } catch (error) {
-      console.log(error);
+              progress.report({
+                increment: branches.length ? 10 : 90,
+                message: message,
+              });
+              window.showInformationMessage(message);
+              return;
+            },
+          );
+        };
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
-  let disposable = commands.registerCommand(commadId, commad);
+    let disposable = commands.registerCommand(commadId, commad);
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
+  }
 }
 
 function initStatusBar(context) {
