dcc6eafb4f904172cafab6b2aa74fc7c4884e791
John <mrooze@v-hiker.cn>
2021-11-18 12:16:22 +0800

    check git

--- src/git.js
+++ src/git.js
@@ -35,9 +35,8 @@ module.exports = class Git {
     return stdout.toString().trim();
   }
   static checkGit() {
-    const { status, stderr, stdout } = spawnSync("git", ["version"]);
-    console.log(stderr, stdout, "===");
-    return !(status === null);
+    const { stdout } = spawnSync("git", ["version"]);
+    return stdout && /(git|version)/gi.test(stdout.toString());
   }
   checkRemoteBranchExist(branch = "") {
     branch = branch || this.getCurrentBranch();
